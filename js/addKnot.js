df3e4d6beae6d96f3253aed139d0fa5ca475693e
David Jiang <dnjiang@stanford.edu>
2015-03-01 14:51:15 -0800

    done

--- js/addKnot.js
+++ js/addKnot.js
@@ -1,6 +1,9 @@
 
 $(document).ready(function() {
-	updateContentPane();
+    var seedData= '<article class="knotMember" data-knots="[13,20,6]" data-goal="15"><h3>Read for fun<br><small>with Mike</small></h3><span class="days">47 days left</span><section class="memberDetail" style="display: none;"><input type="range" class="logSlider" min="0/" max="9"><div class="rangeVal">1</div> <button class="logConfirm">ok</button></section><div class="colorBlocks" style="position:absolute; height:100%; top: 0; background:#2980b9; width:13%; left:0%"></div><div class="colorBlocks" style="position:absolute; height:100%; top: 0; background:#27ae60; width:20%; left:13%"></div><div class="colorBlocks" style="position:absolute; height:100%; top: 0; background:#2980b9; width:6%; left:33%"></div></article><article class="knotMember" data-knots="[20,40]" data-goal="5"><h3>Swim<br><small>with Wending</small></h3><span class="days">47 days left</span><section class="memberDetail" style="display: none;"><input type="range" class="logSlider" min="0/" max="2"><div class="rangeVal">2</div> <button class="logConfirm">ok</button></section><div class="colorBlocks" style="position:absolute; height:100%; top: 0; background:#2980b9; width:20%; left:0%"></div><div class="colorBlocks" style="position:absolute; height:100%; top: 0; background:#27ae60; width:40%; left:20%"></div></article><article class="knotMember" data-knots="[15,30,15,5,20]" data-goal="20"><h3>Morning Run<br><small>with John</small></h3><span class="days">33 days left</span><section class="memberDetail" style="display: block;"><input type="range" class="logSlider" min="0/" max="3"><div class="rangeVal">4</div> <button class="logConfirm">ok</button></section><div class="colorBlocks" style="position:absolute; height:100%; top: 0; background:#2980b9; width:15%; left:0%"></div><div class="colorBlocks" style="position:absolute; height:100%; top: 0; background:#27ae60; width:30%; left:15%"></div><div class="colorBlocks" style="position:absolute; height:100%; top: 0; background:#2980b9; width:15%; left:45%"></div><div class="colorBlocks" style="position:absolute; height:100%; top: 0; background:#27ae60; width:5%; left:60%"></div><div class="colorBlocks" style="position:absolute; height:100%; top: 0; background:#2980b9; width:20%; left:65%"></div></article>';
+	var orig = sessionStorage.getItem("knots");
+    if(orig.length == 0) sessionStorage.setItem("knots", seedData);
+    updateContentPane();
     function generateOneBlock(width, offset, isGreen) {
         var color = "#27ae60";
         if(!isGreen) color = "#2980b9";
@@ -56,6 +59,7 @@ $(document).ready(function() {
     $("#knots").on("change", ".logSlider", function() {
         var newVal = $(this).val();
         $(this).closest(".knotMember").find('.rangeVal').html(newVal);
+        
     });
 	function getKnotString() {
 		var partner = $("#partnerIn").val();
@@ -70,6 +74,7 @@ $(document).ready(function() {
 	    $(".knotMember").each(function() {
             addBarsToKnot(this);
             updateSlider(this);
+            $(this).find(".memberDetail").slideUp();
         });
     }
     $("#addKnotPane").on("click", ".addKnot",function(e){
@@ -92,6 +97,7 @@ $(document).ready(function() {
     	updateContentPane();
     });
     $("#knots").on("click", ".logConfirm", function(e) {
+        e.stopPropagation();
         var knot = $(this).closest(".knotMember");
         var goal = knot.data('goal');
         var newVal = knot.find(".rangeVal").html();
@@ -99,24 +105,20 @@ $(document).ready(function() {
 
         var oldArr = knot.data('knots');
         oldArr.push(newAsPercent);
-        $(knot).attr("data-knots", oldArr);
+        $(knot).attr("data-knots", "[" + oldArr + "]");
         updateSlider(knot);
         addBarsToKnot(knot);
+        var curAllKnots = $("#knots").html();
+        console.log(curAllKnots);
+        sessionStorage.setItem("knots", curAllKnots);
+        $(this).closest(".knotMember").find(".memberDetail").slideUp();
     });
     $("#knots").on("click", ".knotMember", function(e) {
         e.preventDefault();
-        
-        var show = $(this).hasClass('activeMember');
-        if (show) {
-          $(this).removeClass('activeMember')
-          $(this).find(".memberDetail").slideUp('slow');
-        }
-        else {
-          $(this).slideDown('slow', function() {
-            $(this).addClass('activeMember');
             $(this).find(".memberDetail").slideDown();
-          });
-        }
-    });
 
+    });
+    $("#notifs").on("click", function(e) {
+        $("#notifCenter").slideToggle();
+    });
 });
